57199797629216d0a512597dc91c0ee8ec610343
Yijun Zhou <125841649+jinxout@users.noreply.github.com>
2025-02-18 23:07:42 -0500

    Save learning progress.
    Calculations toward a Quiz question.

--- 2025_Intro_Statistics/Z_testing.R
+++ 2025_Intro_Statistics/Z_testing.R
@@ -1,8 +1,12 @@
 2*(1-pnorm(3.5, mean=3, sd=0.1))
 
-2*(1-pnorm(5))
+# Given values
+sample_mean <- 25.81
+population_mean <- 25.3
+population_sd <- 1.2
+sample_size <- 16
 
-1-pnorm(32.7, mean=27.8, sd=3.5)
+# Z-value formula
+z_value <- (sample_mean - population_mean) / (population_sd / sqrt(sample_size))
+z_value
 
-
-1-pnorm(32.7, mean=25.81, sd=1.2)
